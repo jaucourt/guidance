2cb5f3175a4b15a38ce11190561d225c39393423
claredmccarthy <claredmccarthy@gmail.com>
2019-06-17 13:17:21 +0100

    config and arable farm birds page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your ELM agreement',
+  serviceName: 'ELM guidance',
 
   // Default port that prototype runs on
   port: '3000',
